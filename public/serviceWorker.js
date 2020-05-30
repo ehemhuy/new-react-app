fa4162cd69563a9a6adf1c7b813d715a29b2acc1
Phạm Quang Huy <56496918+ehemhuy@users.noreply.github.com>
2020-05-31 02:15:40 +0700

    ad
    ad

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -3,6 +3,7 @@ const assets = [
   "index.html"
 ];
 
+const self = this
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
     caches.open(huyCache).then(cache => {
@@ -17,4 +18,19 @@ self.addEventListener("fetch", fetchEvent => {
       return res || fetch(fetchEvent.request);
     })
   );
+});
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(huyCache);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+            
+    )
 });
