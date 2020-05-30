748cd5a5180f4549de6d7d6ac8ff3159e139c814
Phạm Quang Huy <56496918+ehemhuy@users.noreply.github.com>
2020-05-31 01:59:37 +0700

    aa
    asa

---
+++ public/serviceWorker.js
@@ -0,0 +1,22 @@
+const huyCache = "ehemhuy";
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(huyCache).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
