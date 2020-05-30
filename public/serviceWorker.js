16105b8dd522f9d4b92dd6ada1a0e20979631f83
Phạm Quang Huy <56496918+ehemhuy@users.noreply.github.com>
2020-05-31 02:47:40 +0700

    a
    a

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -17,22 +17,7 @@ self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
     caches.match(fetchEvent.request).then(res => {
       return res || fetch(fetchEvent.request)
-       .catch(() => caches.match('index.html'))
+       .catch(() => caches.match('offline.html'))
     })
   );
-});
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [];
-    cacheWhitelist.push(huyCache);
-
-    event.waitUntil(
-        caches.keys().then((cacheNames) => Promise.all(
-            cacheNames.map((cacheName) => {
-                if(!cacheWhitelist.includes(cacheName)) {
-                    return caches.delete(cacheName);
-                }
-            })
-        ))
-            
-    )
 });
