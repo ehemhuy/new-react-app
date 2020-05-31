133437d452d49ae7124a20665a32fb75b5fd2b8e
Phạm Quang Huy <56496918+ehemhuy@users.noreply.github.com>
2020-05-31 21:44:38 +0700

    Update serviceWorker.js

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,15 @@
 const huyCache = "ehemhuy";
 const assets = [
-  "index.html",
-  "offline.html"
+  "/",
+  "/index.html",
+  "/offline.html",
+  "/offline/1.png",
+  "/offline/2.png",
+  "/offline/3.png",
+  "/offline/4.png",
+  "/offline/5.png",
+  "/offline/6.png",
+  "/offline/bg.png"
 ];
 
 const self = this
