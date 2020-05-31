e37feea7379ef514e0694dbd0bee1dcc3d4084d9
Phạm Quang Huy <56496918+ehemhuy@users.noreply.github.com>
2020-05-31 21:39:16 +0700

    ads

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,14 +1,7 @@
 const huyCache = "ehemhuy";
 const assets = [
   "index.html",
-  "offline.html",
-  "/offline/1.png",
-  "/offline/2.png",
-  "/offline/3.png",
-  "/offline/4.png",
-  "/offline/5.png",
-  "/offline/6.png",
-  "/offline/bg.png"
+  "offline.html"
 ];
 
 const self = this
